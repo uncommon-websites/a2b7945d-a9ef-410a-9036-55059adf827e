94b4b7a2f91c900b9903e0ec47abb3857204c206
Uncle Common <krishnerkar@gmail.com>
2025-05-27 11:51:03 +0000

    Generated website updates for version: 179

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
 	
 	<CustomerCardHero
 	  centered={true}
-	  title="Heavy machiner that thinks ahead"
+	  title="Heavy machinery that thinks ahead"
 	  subtitle="Making heavy minds intelligent, safe, and ready for every tough task. Reduce downtime by 30% while keeping people safe."
 	  callsToAction={[
 	    { href: '/book-demo', label: 'Book a demo' },
